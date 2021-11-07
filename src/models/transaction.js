ca259c4aaffeacd7e9f00200a06cbd047d0c870a
yosael <yosael.gutierrez@gmail.com>
2021-11-06 18:04:05 -0600

    Add currency to transactions list

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -8,21 +8,24 @@ Transaction.getAllByUser = (data) => {
     const userId = [...data];
     const QUERY = `
     with transactions as (
-        select inc.income_id as transaction_id,inc.account_id,cat.category_name,inc.amount,inc.created_date,'income' as type from income inc, account ac,category cat where 1=1
+        select inc.income_id as transaction_id,inc.account_id,cat.category_name,inc.amount,inc.created_date,'income' as type,curr.description as currency from income inc, account ac,category cat,currency curr where 1=1
         and inc.account_id = ac.account_id 
         and inc.category_id = cat.category_id
+        and ac.currency_id = curr.currency_id
         and ac.user_id =$1
         union all
-        select expe.expense_id as transaction_id,expe.account_id,cat.category_name,expe.amount,expe.created_date,'expense' as type from expense expe, account ac, category cat where 1=1 
+        select expe.expense_id as transaction_id,expe.account_id,cat.category_name,expe.amount,expe.created_date,'expense' as type,curr.description as currency from expense expe, account ac, category cat,currency curr where 1=1 
         and expe.account_id = ac.account_id 
         and expe.category_id = cat.category_id
+        and ac.currency_id = curr.currency_id
         and ac.user_id =$1
         union all
-        select transf.transfer_id as transaction_id,transf.account_id_from,'Transfer' as category_name,transf.amount,transf.created_date,'transfer' as type from transfer transf, account ac where 1=1 
+        select transf.transfer_id as transaction_id,transf.account_id_from,'Transfer' as category_name,transf.amount,transf.created_date,'transfer' as type,curr.description as currency from transfer transf, account ac, currency curr where 1=1 
         and transf.account_id_from = ac.account_id 
+        and ac.currency_id = curr.currency_id
         and ac.user_id =$1
         )
-        select * from transactions order by created_date desc,transaction_id desc
+        select * from transactions order by created_date desc
     `;
     return pgdb.query(QUERY,userId);
 }
