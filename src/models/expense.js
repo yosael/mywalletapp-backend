462e9202d005c1441ee2e6c5bdaa8acf938f735b
yosael <yosael.gutierrez@gmail.com>
2021-11-08 19:58:11 -0600

    remove group by currency from report expense

--- src/models/expense.js
+++ src/models/expense.js
@@ -14,12 +14,12 @@ Expense.create = (data) => {
 Expense.getExpensesReport = (data) => {
     const values = [...data];
     return pgdb.query(`
-        select cat.category_name,sum(expe.amount) as total,curr.description as currency from expense expe, account ac, category cat,currency curr where 1=1 
+        select cat.category_name,sum(expe.amount) as total from expense expe, account ac, category cat,currency curr where 1=1 
         and expe.account_id = ac.account_id 
         and expe.category_id = cat.category_id
         and ac.currency_id = curr.currency_id
         and ac.user_id =$1 
-        group by cat.category_name,curr.description
+        group by cat.category_name
     `,values);
 }
 
@@ -27,12 +27,12 @@ Expense.getExpensesReport = (data) => {
 Expense.getExpensesReportLastGroupByDate = (data) => {
     const values = [...data];
     return pgdb.query(`
-        select expe.created_date,sum(expe.amount) as total,curr.description as currency from expense expe, account ac, currency curr where 1=1 
+        select expe.created_date,sum(expe.amount) as total from expense expe, account ac, currency curr where 1=1 
         and expe.account_id = ac.account_id 
         and ac.currency_id = curr.currency_id
         and ac.user_id =$1 
         and expe.created_date >  CURRENT_DATE - INTERVAL '1 months'
-        group by expe.created_date,curr.description
+        group by expe.created_date
     `,values);
 }
 
